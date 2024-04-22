57192c19f11a321bfb13315b8a57083f689e5f19
Jeremy Faden <faden@cottagesystems.com>
2024-04-22 08:01:40 -0500

    multi-byte read implemented for ConcatenateInputStream

--- java/src/hapi/cache/ConcatenateInputStream.java
+++ java/src/hapi/cache/ConcatenateInputStream.java
@@ -34,6 +34,31 @@ public class ConcatenateInputStream extends InputStream {
     }
 
     @Override
+    public int read(byte[] b, int off, int len) throws IOException {
+        if ( currentStream==null ) {
+            currentStream= streams[currentStreamIndex].openInputStream();
+        }
+        while ( currentStreamIndex<streams.length ) {
+            int l= currentStream.read( b, off, len );
+            if ( l==-1 ) {
+                currentStream.close();
+                currentStreamIndex++;
+                if ( currentStreamIndex < streams.length ) {
+                   currentStream= streams[currentStreamIndex].openInputStream();
+                }
+            } else {
+                return l;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public int read(byte[] b) throws IOException {
+        return read( b, 0, b.length );
+    }
+    
+    @Override
     public void close() throws IOException {
 
     }
